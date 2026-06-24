f1b8fe736a63fc39feafc66dfb5d8b1ca7558c7e
Goodsome <xlxmesh@gmail.com>
2026-06-24 18:30:07 +0800

    Remove unused codegen CLI commands

--- apps/pangu_cli/src/pangu_cli/main.py
+++ apps/pangu_cli/src/pangu_cli/main.py
@@ -3,15 +3,6 @@ from pathlib import Path
 import typer
 from foundation.logging_setup import configure_logging
 from pangu_cli.container import create_container
-from codegen.code_metadata.interfaces.cli.clean_node import clean_node
-from codegen.code_metadata.interfaces.cli.generate_code import generate_code
-from codegen.code_metadata.interfaces.cli.get_code_node import get_code_node
-from codegen.code_metadata.interfaces.cli.get_dev_progress import get_dev_progress
-from codegen.code_metadata.interfaces.cli.ingest_project import ingest_project
-from codegen.code_metadata.interfaces.cli.list_unused_nodes import list_unused_nodes
-from codegen.code_metadata.interfaces.cli.listen import listen
-from codegen.code_metadata.interfaces.cli.move_node import move_node, move_to_ast_stmt
-from codegen.code_metadata.interfaces.cli.rename_node import rename_node
 from pangu_cli.run_outbox_worker import run_worker
 from architecture.interfaces.cli import arch_app
 from code_dom.interfaces.cli import code_dom_app
@@ -24,17 +15,7 @@ app = typer.Typer(
 )
 app.add_typer(arch_app, name="arch")
 app.add_typer(code_dom_app, name="dom")
-app.command()(generate_code)
-app.command()(get_dev_progress)
-app.command()(clean_node)
-app.command()(ingest_project)
-app.command()(get_code_node)
-app.command()(list_unused_nodes)
 app.command()(run_worker)
-app.command()(listen)
-app.command()(move_node)
-app.command()(move_to_ast_stmt)
-app.command()(rename_node)
 
 
 def main():
