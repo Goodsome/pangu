8d21b9424ce5bd2e70d03c6dcd162d05fc8e9888
Goodsome <xlxmesh@gmail.com>
2026-06-24 18:54:59 +0800

    Remove unused integration events and clean unused nodes command

--- foundation/src/foundation/integration_events/node_moved.py
+++
@@ -1,9 +0,0 @@
-from typing import ClassVar
-from codegen.code_metadata.domain.core.fqn import Fqn
-from foundation.building_blocks.event import IntegrationEvent
-
-
-class NodeMovedIntegrationEvent(IntegrationEvent):
-    __domain_entity__: ClassVar[str] = "code_node"
-    old_fqn: Fqn
-    new_fqn: Fqn
