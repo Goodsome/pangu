37b2af1aa90dbebf1aa821e1e16695f70ac58999
Goodsome <xlxmesh@gmail.com>
2026-07-11 15:09:12 +0800

    Remove stale code_metadata package `__init__` exports

--- src/codegen/code_metadata/domain/enums/__init__.py
+++
@@ -1,8 +0,0 @@
-from codegen.code_metadata.domain.enums.ast_expr_kind import AstExprKind
-from codegen.code_metadata.domain.enums.ast_match_pattern_kind import (
-    AstMatchPatternKind,
-)
-from codegen.code_metadata.domain.enums.ast_stmt_kind import AstStmtKind
-from codegen.code_metadata.domain.enums.ast_type_param_kind import AstTypeParamKind
-
-__all__ = ["AstExprKind", "AstMatchPatternKind", "AstStmtKind", "AstTypeParamKind"]
