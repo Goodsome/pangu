01a50f148a8661271479b837da097c91622ee738
Goodsome <xlxmesh@gmail.com>
2026-07-11 15:40:30 +0800

    Remove unused domain layer files

--- src/codegen/code_metadata/domain/domain_events/__init__.py
+++
@@ -1,3 +0,0 @@
-from codegen.code_metadata.domain.domain_events.node_deleted import NodeDeleted
-
-__all__ = ["NodeDeleted"]
--- src/codegen/code_metadata/domain/domain_events/node_deleted.py
+++
@@ -1,8 +0,0 @@
-from codegen.code_metadata.domain.core.fqn import Fqn
-from codegen.code_metadata.domain.enums.code_node_kind import CodeNodeKind
-from foundation.building_blocks.event import DomainEvent
-
-
-class NodeDeleted(DomainEvent):
-    node_id: Fqn
-    node_kind: CodeNodeKind
