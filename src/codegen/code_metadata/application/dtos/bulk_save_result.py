ad8aa1193190e1f99db79513e149deabe4febda1
Goodsome <xlxmesh@gmail.com>
2026-07-11 14:46:40 +0800

    Remove unused code_metadata module

--- src/codegen/code_metadata/application/dtos/bulk_save_result.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class BulkSaveResult(BaseModel):
-    """save_nodes_bulk 的执行结果。"""
-
-    nodes_upserted: int
-    edges_created: int
--- src/codegen/code_metadata/application/unit_of_work.py
+++
@@ -1,4 +0,0 @@
-from codegen.code_metadata.domain.ports.code_node_repository import CodeNodeRepository
-from foundation.persistence.ports.unit_of_work import UnitOfWork as BaseUnitOfWork
-
-UnitOfWork = BaseUnitOfWork[CodeNodeRepository]
