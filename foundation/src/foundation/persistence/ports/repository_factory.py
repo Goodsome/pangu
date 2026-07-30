dc7c6d31fa2b967eaf6cc4a98bfc4103daca192f
Goodsome <xlxmesh@gmail.com>
2026-07-30 12:19:20 +0800

    Remove unused repository factory and unit of work ports
    These protocol and abstract class definitions were not being used
    anywhere
    in the codebase.

--- foundation/src/foundation/persistence/ports/repository_factory.py
+++
@@ -1,7 +0,0 @@
-from typing import Protocol, Any
-from sqlalchemy.orm import Session
-from foundation.persistence.ports.repository import Repository
-
-
-class RepositoryFactory[T: Repository[Any, Any]](Protocol):
-    def __call__(self, session: Session) -> T: ...
